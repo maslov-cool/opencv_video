55e1abc9d806a502522488e9f28d53e1186ad676
alex <106116143+maslov-cool@users.noreply.github.com>
2023-08-21 00:27:23 +0300

    Начал изучать opencv-библиотека для создания нейросетей, здесь моя работа с работа с видео.

---
+++ видео.py
@@ -0,0 +1,12 @@
+import cv2
+
+video = cv2.VideoCapture('videos/wind-turbines-in-the-sea.mp4')
+
+while True:
+    success, img = video.read()
+    cv2.imshow('Water', img)
+
+    key = cv2.waitKey(10)
+
+    if key == ord('c'):
+        break
